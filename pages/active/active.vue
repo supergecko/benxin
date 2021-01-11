163079a6fd76852626f1856dac43604bb1ffa8f1
smallWeed <qqppggttyy@163.com>
2021-01-11 21:52:59 +0800

    增加页面

--- pages/active/active.vue
+++ pages/active/active.vue
@@ -1,8 +1,114 @@
 <template>
+	<view class="bg">
+		<view class="midItem">
+			<view class="itemHeader">
+				<image src="../../static/home/leftyun.png" style="width: 60upx;height: 32upx;margin-right: 35upx;"></image>
+				<view>完善信息</view>
+				<image src="../../static/home/rightyun.png" style="width: 60upx;height: 32upx;margin-left: 35upx;"></image>
+			</view>
+
+			<view class="itemInput">
+				<view class="inputWarp">
+					<view><text style="font-weight: 400;">您的姓名:</text></view>
+					<view style="margin-left: 28upx;">
+						<input focus placeholder="请输入您的姓名" style="font-size: 32upx;"/>
+					</view>
+				</view>
+				<view class="bottomLine"></view>
+			</view>
+			<view>
+				<view class="inputWarp">
+					<view><text style="font-weight: 400;">性&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;别:</text></view>
+					<view style="margin-left: 28upx;">
+						<radio-group name="gender" style="font-size: 32upx;transform:scale(0.8);">
+							<label style="margin-right: 82upx;">
+								<radio value="男" color="#9D887C"/><text>男</text>
+							</label>
+							<label>
+								<radio value="女" color="#9D887C"/><text>女</text>
+							</label>
+						</radio-group>
+					</view>
+				</view>
+				<view class="bottomLine"></view>
+			</view>
+			<view>
+				<view class="inputWarp">
+					<view><text style="font-weight: 400;">出生日期:</text></view>
+					<view style="margin-left: 28upx;">
+						<input focus placeholder="请选择出生日期" style="font-size: 32upx;"/>
+					</view>
+				</view>
+				<view class="bottomLine"></view>
+			</view>
+		</view>
+		<view class="bottomBtn">立即测算</view>
+	</view>
 </template>
 
 <script>
 </script>
 
 <style>
+	
+	.bg {
+		background-image: url(../../static/home/bg.png);
+		background-size: 100% 100%;
+		width: 750upx;
+	}
+
+	.bottomBtn {
+		background-image: url(../../static/home/button.png);
+		width: 370upx;
+		height: 98upx;
+		margin: 0 auto;
+		background-size: 100% 100%;
+		line-height: 90upx;
+		text-align: center;
+		font-size: 34upx;
+		color: #090500;
+		font-weight: 400;
+		margin-top: -68upx;
+	}
+
+	.midItem {
+		background-image: url(../../static/home/midbg.png);
+		background-size: 100% 100%;
+		width: 674upx;
+		height: 634upx;
+		margin: 0 auto;
+		margin-top: 255upx;
+	}
+
+	.itemHeader {
+		font-size: 40upx;
+		font-family: PingFang SC;
+		color: #A07348;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding-top: 60upx;
+	}
+
+	.inputWarp {
+		display: flex;
+		font-size: 32upx;
+		color: #331C01;
+		width: 560upx;
+		margin: 0 auto;
+		padding-top: 49upx;
+		font-family: PingFang SC;
+	}
+
+	.uni-input-placeholder {
+		color: #B28B65;
+	}
+
+	.bottomLine {
+		width: 558upx;
+		height: 1upx;
+		background-color: #B28B65;
+		margin: 0 auto;
+		margin-top: 36upx;
+	}
 </style>
--- pages/message/message.vue
+++ pages/message/message.vue
@@ -1,8 +1,148 @@
 <template>
+	<view style="width: 100%;height: 100%;display: flex;">
+		<!-- 头部背景 -->
+		<view class="headWarp">
+			<view class="headItem">
+				<!-- 头部名字 -->
+				<view class="fullName">
+					<view class="NameWarp">
+						<view style="font-size: 36upx;">姓名：何先生(男命)</view>
+						<view>公历：1979 年 3 月 23 日 0 时 0 分 </view>
+						<view>农历：一九七九年二月廿五日子时</view>
+					</view>
+				</view>
+				<!-- 头部名字 -->
+				
+				<!-- 表格测算 -->
+				<view class="logicWarp">
+
+				</view>
+				<!-- 表格测算 -->
+			</view>
+			
+			<!-- 五行健康 -->
+			<view class="fiveElementsWarp">
+				<view class="fiveText">五行健康</view>
+				<table style="width: 606upx;height: 152upx;text-align: center;border-collapse:collapse;margin: 0 auto;">
+					<tr>
+						<td>自身五行</td>
+						<td>财富子女</td>
+						<td>事业伴侣</td>
+						<td>官鬼疾病</td>
+						<td>贵人父母</td>
+					</tr>
+					<tr>
+						<td>水</td>
+						<td>木</td>
+						<td>火</td>
+						<td>土</td>
+						<td>金</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>2</td>
+						<td>1</td>
+						<td>4</td>
+						<td>6</td>
+					</tr>
+					<tr>
+						<td>弱</td>
+						<td>弱</td>
+						<td>过旺</td>
+						<td>弱</td>
+						<td>过旺</td>
+					</tr>
+				</table>
+			</view>
+			<!-- 五行健康 -->
+			
+			
+		</view>
+		<!-- 头部背景 -->
+		
+		<!-- 中间背景 -->
+		<view class="middleWarp"></view>
+		<!-- 中间背景 -->
+		
+		<!-- 底部背景 -->
+		<view class="bottomWarp"></view>
+		<!-- 底部背景 -->
+	</view>
 </template>
 
 <script>
 </script>
 
 <style>
+	.fiveText {
+
+		font-size: 34upx;
+		font-family: Source Han Serif SC;
+		font-weight: bold;
+		color: #331C01;
+		text-align: center;
+		margin-bottom: 16upx;
+	}
+
+	tr td {
+		border: 1upx solid #331C01;
+		font-family: Source Han Serif SC;
+		font-weight: bold;
+		color: #331C01;
+		font-size: 24upx;
+	}
+
+	.headWarp {
+		background-image: url(../../static/result/bgHead.png);
+		background-size: 100% 100%;
+		width: 750upx;
+		height: 2144upx;
+	}
+
+	.headItem {
+		background-image: url(../../static/result/itemBg.png);
+		background-size: 100% 100%;
+		width: 684upx;
+		height: 920upx;
+		margin: 0 auto;
+		margin-top: 678upx;
+		padding-top: 48upx;
+	}
+
+	.fullName {
+		background-image: url(../../static/result/xingming.png);
+		background-size: 100% 100%;
+		width: 554upx;
+		height: 184upx;
+		margin: 0 auto;
+	}
+
+	.NameWarp {
+		font-size: 28upx;
+		font-family: Source Han Serif SC;
+		font-weight: bold;
+		color: #331C01;
+		line-height: 53upx;
+		padding-top: 18upx;
+		margin-left: 38upx;
+	}
+
+	.logicWarp {
+		background-image: url(../../static/result/head.png);
+		background-size: 100% 100%;
+		width: 488upx;
+		height: 626upx;
+		margin: 0 auto;
+		margin-top: 49upx;
+	}
+
+	.fiveElementsWarp {
+		background-image: url(../../static/result/wuxingBg.png);
+		background-size: 100% 100%;
+		width: 664upx;
+		height: 250upx;
+		margin: 0 auto;
+		margin-top: 44upx;
+		padding-top: 21upx;
+	}
 </style>
